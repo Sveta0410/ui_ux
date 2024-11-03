3c1d818f139695fe54c32518e241a8e63243c6d3
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-11-03 20:46:59 +0300

    header done

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import styles from './header.module.css';
 import React from 'react';
-import { Flower } from 'lucide-react';
+import { Flower, Heart, ShoppingBag  } from 'lucide-react';
 
 export const Header = () => {
     const navigate = useNavigate();
@@ -9,15 +9,15 @@ export const Header = () => {
     return (
         <>
             <header className={styles.header}>
-            
+                <div className={styles.headerName}>
+                    <Flower color="#ffffff"  size={50} strokeWidth={1}/>
+                    <Link to="/" className={styles.headerTitle}>SofToy</Link>
+                </div>
                 <nav>
                 
                     <ul className={styles.navList}>
                     
-                        <div className={styles.headerName}>
-                            <Flower color="#ffffff"  size={50} strokeWidth={1}/>
-                            <Link to="/" className={styles.headerTitle}>SofToy</Link>
-                        </div>
+
                     
                         <li className={styles.navItem}>
                             <button onClick={() => navigate('/catalog')}>Каталог</button>
@@ -32,6 +32,10 @@ export const Header = () => {
 
                     </ul>
                 </nav>
+                <div className={styles.headerIcons}>
+                    <Heart className= {styles.headerIcon} color="#FFD7DE"  size={40} strokeWidth={1} />
+                    <ShoppingBag className= {styles.headerIcon} color="#ffffff"  size={40} strokeWidth={1}/>
+                </div>
             </header>
            
         </>
