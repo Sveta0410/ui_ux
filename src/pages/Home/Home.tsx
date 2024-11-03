c95d327df89bd93f7359378a9c54f924696292a8
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-11-03 22:02:00 +0300

    минимум для главной странички

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,38 +6,11 @@ import mainToy from '/src/assets/doll.jpg';
 
 function Home() {
  return (
-    /* <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', height: '75vh'}}> */
     <div >
         <div className={styles.mainSection}>
             <p className={styles.name}>SofToy</p>
             <img src={mainToy} alt="MainToy" className={styles.mainImage}></img>
         </div>
-        {/* <Space align="center"> */}
-        <Flex gap="middle" justify='space-around' align='center'>
-        <Col span={32}></Col>
-        <Title style = {{fontFamily: 'Montez', color:'white', fontSize: '85px'}}>SoftToy</Title>
-        <Col span={32}></Col>
-        <Image
-            width={400}
-            style = {{borderRadius: '30px'}}
-            src="/doll.jpg"
-        />
-        <Col span={32}></Col>
-  </Flex>
-  <h1></h1>
-  <p>, </p>
-  <p></p>
-  <p>, </p>
-  <p></p>
-  <p> .</p>
-  <p></p>
-  <p>, </p>
-  <p></p>
-  <p>, </p>
-  <p></p>
-  <p> .</p>
-  <p></p>
-  {/* </Space> */}
     </div>
  );
 }
