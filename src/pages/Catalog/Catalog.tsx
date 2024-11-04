c3068b0e83cf63d127bccab7267285202b7eab34
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-11-04 17:40:42 +0300

    started making catalog

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,6 +1,7 @@
 import { Button } from "antd";
 import React from "react";
 import styles from './catalog.module.css';
+import toy_0 from '/src/assets/doll.jpg';
 import { Link } from "react-router-dom";
 
 function Catalog() {
@@ -9,7 +10,28 @@ function Catalog() {
             <div className={styles.catalog}>
                 <h1>Каталог</h1>
                 <p><Link to="/product" >Товар</Link></p>
+                <div className={styles.rectangle}>
+                <img src={toy_0} alt="toy" className={styles.image}></img>
+                <p className={styles.price}>2500 ₽</p>
+                <p className={styles.description}>Кукла Розалин</p>
             </div>
+            <div className={styles.rectangle}>
+                <img src={toy_0} alt="toy" className={styles.image}></img>
+                <p className={styles.price}>2500 ₽</p>
+                <p className={styles.description}>Кукла Розалин</p>
+            </div>
+            <div className={styles.rectangle}>
+                <img src={toy_0} alt="toy" className={styles.image}></img>
+                <p className={styles.price}>2500 ₽</p>
+                <p className={styles.description}>Кукла Розалин</p>
+            </div>
+            <div className={styles.rectangle}>
+                <img src={toy_0} alt="toy" className={styles.image}></img>
+                <p className={styles.price}>2500 ₽</p>
+                <p className={styles.description}>Кукла Розалин</p>
+            </div>
+            </div>
+            
        </div>
     );
    }
