76aa82e58c6f10b5ccc9376c42d2346d3b20a439
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-11-06 16:36:12 +0300

    started updating home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Col, Flex, Image, Space, Typography  } from 'antd';
-const { Title } = Typography;
 import styles from './home.module.css';
 import mainToy from '/src/assets/doll.jpg';
+import frogBest from '/src/assets/bestToys/frogBest.svg';
+import bunnyBest from '/src/assets/bestToys/bunnyBest.svg';
+import smallBunnyBest from '/src/assets/bestToys/smallBunnyBest.svg';
 
 function Home() {
  return (
-    <div >
+    <div>
         <div className={styles.mainSection}>
             <p className={styles.name}>SofToy</p>
             <img src={mainToy} alt="MainToy" className={styles.mainImage}></img>
         </div>
+        <div className={styles.bestHeader}>
+
+        </div>
+        <img src={frogBest} alt="MainToy" className={styles.mainImage}></img>
+        <img src={bunnyBest} alt="MainToy" className={styles.mainImage}></img>
+        <img src={smallBunnyBest} alt="MainToy" className={styles.mainImage}></img>
     </div>
  );
 }
