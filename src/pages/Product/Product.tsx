b7687b8477af4c21a7f52f10c45450015838469c
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-11-06 18:57:46 +0300

    added simple api (getting random name)

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './product.module.css';
 import { useParams } from 'react-router-dom';
 import { Products } from '../../consts/products';
@@ -6,11 +6,39 @@ import { Products } from '../../consts/products';
 
 
 function Product() {
+    const [fullName, setFullName] = useState("");
+    useEffect(() => {
+        async function getRandomFullName() {
+            try {
+                const response = await fetch('https://randomuser.me/api/');
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+
+                const firstName = data.results[0].name.first;
+                const lastName = data.results[0].name.last;
+                
+                const fullName = `${firstName} ${lastName}`;
+
+                setFullName(fullName);
+            } catch (error) {
+                console.error('Ошибка при получении имени:', error);
+            }
+        }
+
+        // Вызываем функцию один раз при монтировании компонента
+        getRandomFullName();
+    }, []); // Пустой массив зависимостей означает, что useEffect вызывается только один раз при монтировании
+
     const {id} = useParams();
     
     return (
         <div className={styles.productPage}>
             <h1>{id}</h1>
+            <p>Автор схемы: {fullName}</p>
             <p>{Products.find(product => product.id === Number(id))?.description}</p>
             
             
