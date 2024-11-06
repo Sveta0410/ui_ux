b655aa8ebbb0d5868f095efc4c0fc24f7e3b0b97
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-11-06 20:50:50 +0300

    home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import styles from './home.module.css';
 import mainToy from '/src/assets/doll.jpg';
+import aboutUsToy from '/src/assets/bestToys/smallBunnyBest.svg';
 import BestProduct from "../../components/bestProduct";
 import { BestProducts } from "../../consts/bestProducts";
+import { AboutUsText } from "../../consts/aboutUs";
 
 function Home() {
  return (
     <div>
         <div className={styles.mainSection}>
             <p className={styles.name}>SofToy</p>
-            <img src={mainToy} alt="MainToy" className={styles.mainImage}></img>
+            <img src={mainToy} alt="MainToy" className={styles.mainImage}/>
         </div>
         <div className={styles.bestSection}>
             <div className={styles.bestHeaderBlock}>
-                <p  className={styles.bestHeader}>Лучшие товары</p>
+                <p  className={styles.homeHeader}>Лучшие товары</p>
             </div>
             <div className={styles.bestProducts}>
                 {BestProducts.map((product) => (
@@ -23,6 +25,17 @@ function Home() {
                         title={product.title}
                     />
                     ))}
+            </div>
+        </div>
+        <div className={styles.aboutUsSection}>
+            <img src={aboutUsToy} className={styles.aboutUsImage}/>
+            <div className={styles.aboutUsTextSection}>
+                <div className={styles.aboutHeaderBlock}>
+                    <p className={styles.homeHeader}>О нас</p>
+                </div>
+                <div className={styles.aboutTextBlock}>
+                    <p className={styles.homeText}>{AboutUsText}</p>
+                </div>
             </div>
         </div>
     </div>
