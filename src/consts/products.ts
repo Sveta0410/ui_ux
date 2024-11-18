1ceb72bb2c154c72be1c4565bdcdd50a6f37fbe5
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-11-18 13:06:12 +0300

    added descriptions

--- src/consts/products.ts
+++ src/consts/products.ts
@@ -11,77 +11,77 @@ export const Products = [
       id: 2,
       price: 350,
       url: '/src/assets/toys/dino.png',
-      description: ['Описание продукта 2']
+      description: ['Авторская игрушка ручной работы', 'Размер: 10*7 см']
     },
     {
       title: 'Мышка',
       id: 3,
       price: 350,
       url: '/src/assets/toys/mouse.png',
-      description: ['Описание продукта 3']
+      description: ['Авторская игрушка ручной работы', 'Размер: 5*10 см']
     },
     {
       title: 'Мини лягушка',
       id: 4,
       price: 250,
       url: '/src/assets/toys/tiny_frog.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 5*5 см']
     },
     {
       title: 'Кукла Розалин',
       id: 5,
       price: 2500,
       url: '/src/assets/toys/doll.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 20*10 см']
     },
     {
       title: 'Кролик малыш',
       id: 6,
       price: 2000,
       url: '/src/assets/toys/tiny_bunny.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 10*7 см']
     },
     {
       title: 'Веселая лягушка',
       id: 7,
       price: 400,
       url: '/src/assets/toys/frog.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 15*10 см']
     },
     {
       title: 'Подставка весенняя',
       id: 8,
       price: 1000,
       url: '/src/assets/toys/flower_coaster.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 9*9 см']
     },
     {
       title: 'Медуза Марина',
       id: 9,
       price: 700,
       url: '/src/assets/toys/octopus.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 3*7 см']
     },
     {
       title: 'Лягушка-квакушка',
       id: 10,
       price: 2300,
       url: '/src/assets/toys/frog_small.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 10*5 см']
     },
     {
       title: 'Мишка',
       id: 11,
       price: 1000,
       url: '/src/assets/toys/bear.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 15*7 см']
     },
     {
       title: 'Зайка Настя',
       id: 12,
       price: 1700,
       url: '/src/assets/toys/bunny.png',
-      description: ['Описание продукта 4']
+      description: ['Авторская игрушка ручной работы', 'Размер: 30*20 см']
     },
 
   ];
