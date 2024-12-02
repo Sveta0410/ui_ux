332446b82822e38b2ac60eeb0106141abd125a87
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-12-02 13:55:32 +0300

    added owl to contacts

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import styles from './contacts.module.css';
+import owl from '/images/owl1.png';
 
 function Contacts() {
  return (
     <div>
         <div className={styles.mainSection}>
-            <p className={styles.name}>Контактная информация</p>
-            <p className={styles.title}>Для заказа игрушек обращайтесь по телефону</p>
-            <p className={styles.info}>+7 (915) 225 78 32</p>
-            <p className={styles.title}>Идеи и предложения отправляйте на почту</p>
-            <p className={styles.info}>ideas@softoy.ru</p>
-            <p className={styles.title}>По вопросам трудоустройства</p>
-            <p className={styles.info}>+7 (915) 588 99 32</p>
-            <p className={styles.info}>work@softoy.ru</p>
+            <div className={styles.imageSection}>
+                <img src={owl} className={styles.contactsImage}/>
+            </div>
+            <div className={styles.infoSection}>
+                <p className={styles.name}>Контактная информация</p>
+                <p className={styles.title}>Для заказа игрушек обращайтесь по телефону</p>
+                <p className={styles.info}>+7 (915) 225 78 32</p>
+                <p className={styles.title}>Идеи и предложения отправляйте на почту</p>
+                <p className={styles.info}>ideas@softoy.ru</p>
+                <p className={styles.title}>По вопросам трудоустройства</p>
+                <p className={styles.info}>+7 (915) 588 99 32</p>
+                <p className={styles.info}>work@softoy.ru</p>
+            </div>
+
         </div>
+        
+        
     </div>
  );
 }
